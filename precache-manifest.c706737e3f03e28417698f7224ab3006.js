8aa6f1d7747c85a21703587efb66ae76915b0250
Niraja Shakya <nirajashakya@Nirajas-MacBook-Air.local>
2019-12-27 01:30:18 +0100

    Updates

---
+++ precache-manifest.c706737e3f03e28417698f7224ab3006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b437585c1639c6569e0bd9265655c57",
+    "url": "/hydrane-case-study/index.html"
+  },
+  {
+    "revision": "91ce2e7b48b8e26c6f16",
+    "url": "/hydrane-case-study/static/css/2.f76af334.chunk.css"
+  },
+  {
+    "revision": "443aa0e617715c9b9628",
+    "url": "/hydrane-case-study/static/css/main.ae354256.chunk.css"
+  },
+  {
+    "revision": "91ce2e7b48b8e26c6f16",
+    "url": "/hydrane-case-study/static/js/2.ebded62d.chunk.js"
+  },
+  {
+    "revision": "9526f8bda004559bfc4bd1893d406e34",
+    "url": "/hydrane-case-study/static/js/2.ebded62d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "443aa0e617715c9b9628",
+    "url": "/hydrane-case-study/static/js/main.fae21e98.chunk.js"
+  },
+  {
+    "revision": "b89949b6eba4deed1882",
+    "url": "/hydrane-case-study/static/js/runtime-main.07cfe47b.js"
+  }
+]);
